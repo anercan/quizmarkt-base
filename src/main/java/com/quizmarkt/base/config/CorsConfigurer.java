8dc792aef9deffd6d1f617b740d6606070ea0ebd
anercan <anercan@gmail.com>
2024-11-25 22:45:03 +0000

    cors congirurer addition

---
+++ src/main/java/com/quizmarkt/base/config/CorsConfigurer.java
@@ -0,0 +1,29 @@
+package com.quizmarkt.base.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author anercan
+ */
+public class CorsConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://anercan.github.io/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+
+}
+
+
