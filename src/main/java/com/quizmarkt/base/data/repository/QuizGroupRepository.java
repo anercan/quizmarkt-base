64944fbeb401264d6d0060f70b991f5ff167eb22
anercan <anercan@gmail.com>
2025-01-01 20:09:15 +0000

    debug

--- src/main/java/com/quizmarkt/base/data/repository/QuizGroupRepository.java
+++ src/main/java/com/quizmarkt/base/data/repository/QuizGroupRepository.java
@@ -1,6 +1,8 @@
 package com.quizmarkt.base.data.repository;
 
+import com.quizmarkt.base.data.constant.CacheConstants;
 import com.quizmarkt.base.data.entity.QuizGroup;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,9 +17,9 @@ import java.util.List;
 @Repository
 public interface QuizGroupRepository extends JpaRepository<QuizGroup, Long> {
 
-    //@Cacheable(value = CacheConstants.QUIZ_GROUPS, key = "#appId")
+    @Cacheable(value = CacheConstants.QUIZ_GROUPS, key = "#appId")
     @EntityGraph(attributePaths = "attributes")
-    List<QuizGroup> findAllByActiveAndAppIdOrderByPriorityAsc(boolean active, int appId);
+    List<QuizGroup> findAllByActiveAndAppIdOrderByPriorityAsc(boolean active, Integer appId);
     List<QuizGroup> findAllByAppId(int appId, Pageable pageable);
 
 }
